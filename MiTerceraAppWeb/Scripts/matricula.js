ff885de54eb1f64b43702ddeed825f41b9eb79aa
programacionparaaprender <yancel209@gmail.com>
2023-08-06 12:10:51 -0500

    agregando cambios

--- MiTerceraAppWeb/Scripts/matricula.js
+++ MiTerceraAppWeb/Scripts/matricula.js
@@ -1,6 +1,7 @@
 ﻿
 var base_url = window.location.origin;
-const url = base_url +"/Matricula";
+const url = base_url + "/Matricula";
+
 function listar() {
     $.get(url + "/listar", function (data) {
         try {
@@ -35,13 +36,14 @@ function listar() {
     });
 }
 
+
+
 function abrirModal(IID = 0) {
     if (IID == 0) {
         borrarDatos();
         
     } else {
         $.get(url + "/recuperarInformacion?id=" + IID, function (data) {
-
             obtenerPorId('txtIdMatricula').value = data[0].IID;
             obtenerPorId('cboPeriodo').value = data[0].IIDPERIODO;
             obtenerPorId('cboGradoSeccion').value = data[0].IIDGRADOSECCION;
@@ -49,17 +51,17 @@ function abrirModal(IID = 0) {
         });
         $.get(url + '/Cursos?iidmatricula=' + IID, function(data){
             var contenido = "<tbody>";
-            for(var i=0; i<data.length; i++){
+            for(let dat of data){
                 contenido += '<tr>';
                 contenido += '<td>';
-                if(data[i].bhabilitado == 1){
-                    contenido += "<input class='checkbox' type='checkbox' id='"+data[i].IIDCURSO+"' checked='true' />";
-                }else {
-                    contenido += "<input class='checkbox' type='checkbox' id='" + data[i].IIDCURSO +"' />";
+                if(dat.bhabilitado == 1){
+                    contenido += "<input class='checkbox' type='checkbox' id='"+dat.IIDCURSO+"' checked='true' />";
+                } else {
+                    contenido += "<input class='checkbox' type='checkbox' id='" + dat.IIDCURSO +"' />";
                 }
                 contenido += '</td>';
                 contenido += '<td>';
-                contenido += data[i].NOMBRE;
+                contenido += dat.NOMBRE;
                 contenido += '</td>';
                 contenido += '</tr>';
             }
@@ -100,14 +102,21 @@ function agregar() {
                 frm.append('BHABILITADO', 1);
                 frm.append('IIDGRADOSECCION', IIDGRADOSECCION);
 
-                var checkbox = document.getElementsByClassName("checkbox");
+                //var checkbox = document.getElementsByClassName("checkbox");
                 var valorAEnviar;
-                for (let check of checkbox) {
-                    if (check.checked) {
-                        valorAEnviar += check.id;
+                //for (let check of checkbox) {
+                //    if (check.checked) {
+                //        valorAEnviar += check.name;
+                //        valorAEnviar += "$";
+                //    }
+                //}
+                $("input:checkbox[class^='checkbox']").each(function (index, e) {
+                    if ($("input:checkbox[name^='checkbox']").is(":checked")) {
+                        valorAEnviar += $("input:checkbox[name^='checkbox']").attr("id");
                         valorAEnviar += "$";
-                    }
-                }
+                    } // if
+                });
+
                 valorAEnviar = valorAEnviar.substring(0, valorAEnviar.length - 1);
                 frm.append('valorAEnviar', valorAEnviar);
                 $.ajax({
